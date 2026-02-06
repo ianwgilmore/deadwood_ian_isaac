34af07002657e8a512933c00aeda633df97b1909
ianwgilmore <gilmorj9@wwu.edu>
2026-02-05 19:40:56 -0800

    implementing set.java

--- Set.java
+++ Set.java
@@ -1,7 +1,7 @@
 /* 
 (casting office/trailer)
 Attributes
--name()
+-name
 -neighbors[]
 for checking rank up generally false
 -bool castingOffice
@@ -10,11 +10,36 @@ Methods
 -getters and setters
 -
 
-Implemented by - 
-Last Change mm/dd/yy, first
+Implemented by - Ian Gilmore
+Last Change 02/05/26, Ian
+-adding getters setters
 */
 
 public class Set{
+    String name;
+    Set[] neighbors;
+    Boolean castingOffice;
+
+    Public String getName(){
+        return self.name;
+    }
+    Public void setName(newname){
+        self.name = newname;
+    }
+    Public String getNeighbors(){
+        return self.neighbors;
+    }
+    Public void addNeighbors(neighbors){
+        self.neighbors = neighbors;
+    }
+    Public String getcastingOffice(){
+        return self.castingOffice;
+    }
+    Public void getcastingOffice(castingOffice){
+        self.castingOffice = castingOffice;
+    }
+
+
 
 
 }
