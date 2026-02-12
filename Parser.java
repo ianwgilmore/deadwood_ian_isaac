b82b84741f5153d39ce3768d6b36524d5559961a
ravenisa <raveni@wwu.edu>
2026-02-11 18:02:59 -0800

    Starting on Parser.java

--- Parser.java
+++ Parser.java
@@ -1,1 +1,18 @@
 //going to need to parse xml in a way, unsure of what that is going to look like as of now
+
+// Gotten from slides at https://wwu.instructure.com/courses/1854078/files/folder/Reading%20Material/Other%20Topics?preview=145300650
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
+public class Parser {
+    // Guessing what functions will be needed
+    public static void main() {}
+    public static void importDoc() {}
+    public static void printDoc() {} // For checking the doc object is imported properly
+}
