8cec10056dae258b8e4787545277dce7cb967d24
ianwgilmore <gilmorj9@wwu.edu>
2026-02-18 18:02:25 -0800

    working on game loop

--- Game.java
+++ Game.java
@@ -1,1 +1,31 @@
 //gameplay loop
+public class Game{
+    int max_day;
+    int current_day;
+    int scenes_left;
+
+    public static void main(String[] args){
+        //get player input
+        //int n = Controller.getPlayers()
+        int player_num = 2;
+        setup(player_num);
+        Parser parser = new Parser();
+        Board board = new Board();
+        //setup
+        
+
+        while()
+
+
+    }
+
+    private void setup(player_num){
+        //need to set max day, initialize players with correct stats
+        board.buildScenes();
+        board.buildSets();
+        board.buildActingSets();
+        board.buildPlayers(player_num);
+        board.asignScenes();
+
+    }
+}
