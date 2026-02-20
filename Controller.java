bfa881f4832b799b8645afd3928f182ac0703b73
ianwgilmore <gilmorj9@wwu.edu>
2026-02-20 11:03:06 -0800

    general structure for controller

---
+++ Controller.java
@@ -0,0 +1,24 @@
+
+public class Controller{
+
+    public int setup(){
+        int playerNum = view.getPlayerNum();
+        return playerNum;
+    }
+
+    public void act(){
+    }
+
+    public void move(){
+    }
+
+    public void rankUp(){}
+
+    public void takeTurn(){}
+
+    public void nextDay(){}
+
+    public void endTurn(){}
+
+    public void error(){}
+}
