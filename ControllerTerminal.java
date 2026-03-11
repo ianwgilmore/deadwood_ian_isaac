fbc9593bcc9a0e92d894877ac0a9f6e3fee398ff
Ian Gilmore <gilmorj9@wwu.edu>
2026-03-11 13:14:03 -0700

    implementing dynamic buttons

--- ControllerTerminal.java
+++ ControllerTerminal.java
@@ -26,7 +26,7 @@ public class ControllerTerminal implements Controller{
     }
 
     public String takeTurn(ArrayList<String> actions,String name){
-        String action = this.view.getPlayerAction(name);
+        String action = this.view.getPlayerAction(name, actions);
         return action;
     }
 
